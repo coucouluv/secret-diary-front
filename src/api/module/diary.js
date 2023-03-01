4513c7c152c84dc9a737106d950bdf4166ff7196
coucouluv <oy5325@gmail.com>
2023-03-02 01:54:00 +0900

    refactor: 인스턴스 변경

--- src/api/module/diary.js
+++ src/api/module/diary.js
@@ -1,20 +1,20 @@
-import { instance } from '@/api/index';
+import {instanceWithToken} from '@/api/index';
 
 const BASE_URL = '/api/diary';
 
 const diary = {
     getDiaries(request) {
-        const token = localStorage.getItem("accessToken");
-        console.log("token: "+ token)
-        instance.defaults.headers.Authorization = 'Bearer '+token;
-        return instance.get(`${BASE_URL}/info/${request.friendId}?page=${request.page}&size=${request.size}`);
+        return instanceWithToken.get(`${BASE_URL}/info/${request.friendId}?page=${request.page}&size=${request.size}`);
     },
     getDiary(diaryId) {
-        const token = localStorage.getItem("accessToken");
-        console.log("token: "+ token)
-        instance.defaults.headers.Authorization = 'Bearer '+token;
-        return instance.get(`${BASE_URL}/${diaryId}`);
+        return instanceWithToken.get(`${BASE_URL}/${diaryId}`);
     },
+    saveDiary(diaryRequest) {
+        return instanceWithToken.post(`${BASE_URL}`, diaryRequest);
+    },
+    deleteDiary(diaryId) {
+        return instanceWithToken.delete(`${BASE_URL}/${diaryId}`);
+    }
 }
 
 export default diary;
