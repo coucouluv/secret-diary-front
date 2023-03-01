c58311149a1d5c395d65d4b6bc7e01892eb402ce
coucouluv <oy5325@gmail.com>
2023-03-02 01:52:45 +0900

    feat: interceptors를 사용하여 Authorization 헤더 인증 구현

---
+++ src/api/interceptors.js
@@ -0,0 +1,24 @@
+
+export function setInterceptors(instance) {
+	instance.interceptors.request.use(
+		function (config) {
+			const token = localStorage.getItem("accessToken");
+			config.headers.Authorization = `Bearer ${token}`;
+			return config;
+		},
+		function (error) {
+			return Promise.reject(error);
+		},
+	);
+
+	instance.interceptors.response.use(
+		function (response) {
+			return response;
+		},
+		function (error) {
+			return Promise.reject(error);
+		},
+	);
+
+	return instance;
+}
