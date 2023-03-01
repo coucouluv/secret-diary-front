d324c264b725db7575da3338e29a5ad49fca9a1b
coucouluv <oy5325@gmail.com>
2023-03-02 01:54:37 +0900

    feat: 이미지 관련 api 구현

---
+++ src/api/module/image.js
@@ -0,0 +1,26 @@
+import { instance } from '@/api/index';
+
+const BASE_URL = '/api/image';
+
+const image = {
+    upload() {
+        const token = localStorage.getItem("accessToken");
+        console.log("token: "+ token)
+        instance.defaults.headers.Authorization = 'Bearer '+token;
+        return instance.get(`${BASE_URL}/upload`);
+    },
+    share(url) {
+        const token = localStorage.getItem("accessToken");
+        console.log("token: "+ token)
+        instance.defaults.headers.Authorization = 'Bearer '+token;
+        return instance.get(`${BASE_URL}/share/${url}`);
+    },
+    delete(url) {
+        const token = localStorage.getItem("accessToken");
+        console.log("token: "+ token)
+        instance.defaults.headers.Authorization = 'Bearer '+token;
+        return instance.get(`${BASE_URL}/delete/${url}`);
+    }
+}
+
+export default image;
