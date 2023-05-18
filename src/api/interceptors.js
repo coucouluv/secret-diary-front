8d67d0f03ae2acc60349efb5a514e65c634c0a6b
coucouluv <oy5325@gmail.com>
2023-05-18 12:41:06 +0900

    refactor: 토큰 재발급 로직 변경

--- src/api/interceptors.js
+++ src/api/interceptors.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+// import { instanceWithToken } from '@/api/index';
 
 export function setInterceptors(instance) {
   instance.interceptors.request.use(
@@ -13,33 +14,37 @@ export function setInterceptors(instance) {
   );
 
   instance.interceptors.response.use(
-    response => {
+    function (response) {
       return response;
     },
-    async error => {
+    async function (error) {
+      const status = error.response.status;
       const originalRequest = error.config;
-      const request = {
-        refreshToken: localStorage.getItem('refreshToken'),
-      };
-      await axios
-        .post(`${process.env.VUE_APP_API_URL}/api/start/reissue`, request)
-        .then(res => {
-          localStorage.setItem('accessToken', res.data.accessToken);
-          localStorage.setItem('refreshToken', res.data.refreshToken);
-
-          const token = localStorage.getItem('accessToken');
-          originalRequest.headers.Authorization = `Bearer ${token}`;
-          axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-
-          return axios(originalRequest);
-        })
-        .catch(error => {
-          localStorage.removeItem('signin');
-          localStorage.removeItem('accessToken');
-          localStorage.removeItem('refreshToken');
-          alert('유효기간이 만료되었습니다. 다시 로그인 해주세요.');
-          return Promise.reject(error);
-        });
+      console.log('나 여기 옴');
+      if (status === 401) {
+        const request = {
+          refreshToken: localStorage.getItem('refreshToken'),
+        };
+        await axios
+          .post(`${process.env.VUE_APP_API_URL}/api/start/reissue`, request)
+          .then(res => {
+            localStorage.setItem('accessToken', res.data.accessToken);
+            localStorage.setItem('refreshToken', res.data.refreshToken);
+            originalRequest.headers = {
+              Authorization: 'Bearer ' + localStorage.getItem('accessToken'),
+            };
+            console.log(originalRequest);
+            return instance.request(originalRequest);
+          })
+          .catch(error => {
+            localStorage.removeItem('signin');
+            localStorage.removeItem('accessToken');
+            localStorage.removeItem('refreshToken');
+            alert('유효기간이 만료되었습니다. 다시 로그인 해주세요.');
+            return Promise.reject(error);
+          });
+      }
+      return Promise.reject(error);
     },
   );
 
